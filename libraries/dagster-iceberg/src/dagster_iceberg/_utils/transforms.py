67fdf0bd1c9635c2f5961ee7e47942841027d730
Brian Broderick <brian@broderick.io>
2026-08-18 10:49:01 -0400

    Tidying

--- libraries/dagster-iceberg/src/dagster_iceberg/_utils/transforms.py
+++ libraries/dagster-iceberg/src/dagster_iceberg/_utils/transforms.py
@@ -16,7 +16,7 @@ def diff_to_transformation(
     end: dt.datetime
 ) -> transforms.Transform:
     """Based on the interval between two dates, return a transformation"""
-    
+
     start_ = pendulum.instance(start)
     end_ = pendulum.instance(end)
 
